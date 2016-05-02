f144db803a55a212a212801c9cf6af6b199868db
Chris Carroux <ccarroux@calpoly.edu>
2016-05-01 22:53:27 -0700

    Caught up repo to lots of work done offline and restructuring of folder

--- bootstrap/content/index.php
+++ bootstrap/content/index.php
@@ -57,94 +57,6 @@ $dir_name       = explode("/", $this_folder);
 //     $protocol = "http://";
 // }
 
-function rangeDownload($navigation_dir) {
-   $fp = @fopen($navigation_dir, 'rb');
-   $size   = filesize($navigation_dir); // File size
-   $length = $size;           // Content length
-   $start  = 0;               // Start byte
-   $end    = $size - 1;       // End byte
-   // Now that we've gotten so far without errors we send the accept range header
-   /* At the moment we only support single ranges.
-   * Multiple ranges requires some more work to ensure it works correctly
-   * and comply with the spesifications: http://www.w3.org/Protocols/rfc2616/rfc2616-sec19.html#sec19.2
-   *
-   * Multirange support annouces itself with:
-   * header('Accept-Ranges: bytes');
-   *
-   * Multirange content must be sent with multipart/byteranges mediatype,
-   * (mediatype = mimetype)
-   * as well as a boundry header to indicate the various chunks of data.
-   */
-   header("Accept-Ranges: 0-$length");
-   // header('Accept-Ranges: bytes');
-   // multipart/byteranges
-   // http://www.w3.org/Protocols/rfc2616/rfc2616-sec19.html#sec19.2
-   if (isset($_SERVER['HTTP_RANGE'])) {
-      $c_start = $start;
-      $c_end   = $end;
-      // Extract the range string
-      list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
-      // Make sure the client hasn't sent us a multibyte range
-      if (strpos($range, ',') !== false) {
-         // (?) Shoud this be issued here, or should the first
-         // range be used? Or should the header be ignored and
-         // we output the whole content?
-         header('HTTP/1.1 416 Requested Range Not Satisfiable');
-         header("Content-Range: bytes $start-$end/$size");
-         // (?) Echo some info to the client?
-         exit;
-      }
-      // If the range starts with an '-' we start from the beginning
-      // If not, we forward the file pointer
-      // And make sure to get the end byte if spesified
-      if ($range == '-') {
-         // The n-number of the last bytes is requested
-         $c_start = $size - substr($range, 1);
-      }
-      else {
-         $range  = explode('-', $range);
-         $c_start = $range[0];
-         $c_end   = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $size;
-      }
-      /* Check the range and make sure it's treated according to the specs.
-      * http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html
-      */
-      // End bytes can not be larger than $end.
-      $c_end = ($c_end > $end) ? $end : $c_end;
-      // Validate the requested range and return an error if it's not correct.
-      if ($c_start > $c_end || $c_start > $size - 1 || $c_end >= $size) {
-         header('HTTP/1.1 416 Requested Range Not Satisfiable');
-         header("Content-Range: bytes $start-$end/$size");
-         // (?) Echo some info to the client?
-         exit;
-      }
-      $start  = $c_start;
-      $end    = $c_end;
-      $length = $end - $start + 1; // Calculate new content length
-      fseek($fp, $start);
-      header('HTTP/1.1 206 Partial Content');
-      header("foobar: okay");
-   }
-   // Notify the client the byte range we'll be outputting
-   header("Content-Range: bytes $start-$end/$size");
-   header("Content-Length: $length");
-
-   // Start buffered download
-   $buffer = 1024 * 8;
-   while(!feof($fp) && ($p = ftell($fp)) <= $end) {
-      if ($p + $buffer > $end) {
-         // In case we're only outputting a chunk, make sure we don't
-         // read past the length
-         $buffer = $end - $p + 1;
-      }
-      set_time_limit(0); // Reset time limit for big files
-      echo fread($fp, $buffer);
-      flush(); // Free up memory. Otherwise large files will trigger PHP's memory limit.
-   }
-
-   fclose($fp);
-}
-
 if(substr($navigation_dir, -1) != "/"){
     if(file_exists($navigation_dir)){
 
@@ -169,17 +81,7 @@ if(substr($navigation_dir, -1) != "/"){
         ob_clean();
         flush();
         if ($options['general']['read_chunks'] == true) {
-            // readfile_chunked($navigation_dir);
-            if (is_file($navigation_dir)) {
-               // header("Content-type: $mime_type");
-               if (isset($_SERVER['HTTP_RANGE']))  { // do it for any device that supports byte-ranges not only iPhone
-                  rangeDownload($navigation_dir);
-               }
-               else {
-                  header("Content-Length: ".filesize($navigation_dir));
-                  readfile($navigation_dir);
-               }
-            }
+            readfile_chunked($navigation_dir);
         } else {
             readfile($navigation_dir);
         }
@@ -745,7 +647,7 @@ if(($folder_list) || ($file_list) ) {
                 $file_attr = null;
             }
 
-            $table_body .= "<a href=\"" . htmlentities(rawurlencode($item['bname']), ENT_QUOTES, 'utf-8') . "\"$file_attr$file_data$virtual_attr$modified_attr>" . utf8ify($display_name) . "</a>";
+            $table_body .= "<a href=\"" . ("/content" . substr($navigation_dir, 1)) . htmlentities(rawurlencode($item['bname']), ENT_QUOTES, 'utf-8') . "\"$file_attr$file_data$virtual_attr$modified_attr>" . utf8ify($display_name) . "</a>";
 
             // Append checksum info if enabled
             if ( ($options['general']['enable_checksums'] == true) && !empty($options["checksum_files"]) ) {
